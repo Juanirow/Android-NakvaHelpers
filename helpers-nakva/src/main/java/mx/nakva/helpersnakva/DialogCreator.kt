baaa727fad30c014de1fc241de9a7eb3020d66d3
Juancho <linanjm90@gmail.com>
2017-11-04 13:43:32 +0000

    Add a new module to create a quick alert dialogs

---
+++ helpers-nakva/src/main/java/mx/nakva/helpersnakva/DialogCreator.kt
@@ -0,0 +1,101 @@
+package mx.nakva.helpersnakva
+
+import android.content.Context
+import android.content.DialogInterface
+import android.support.v7.app.AlertDialog
+import android.widget.EditText
+import android.widget.LinearLayout
+
+
+/**
+ * Created by Juancho on 04/11/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+object DialogCreator {
+
+    /**
+     *
+     * Display an alert in the current context
+     *
+     * - This dialog can be use as confirmation dialog
+     *
+     * @param context               Current context to show the alert
+     * @param titleId               Title of the alert
+     * @param iconId                Id of the drawable to show as icon
+     * @param positiveBtnTextId     Id of the string to set in the positive button
+     * @param messageId             Message to show in the alert
+     * @param onCLickListener       Listener to detect the clicks in the buttons
+     * @param onDismissListener     Listener to detect the dismiss action of the alert
+     */
+    fun showDialogMessage(context: Context,
+                          titleId: Int,
+                          iconId: Int? = null,
+                          positiveBtnTextId: Int,
+                          messageId: Int,
+                          onCLickListener: DialogInterface.OnClickListener? = null,
+                          onDismissListener: DialogInterface.OnDismissListener? = null) {
+        val builder = AlertDialog.Builder(context)
+        if(iconId != null) {
+            builder.setIcon(iconId)
+        }
+        builder.setTitle(titleId)
+        builder.setPositiveButton(positiveBtnTextId, onCLickListener)
+        builder.setMessage(messageId)
+        val alert = builder.create()
+        alert.setOnDismissListener(onDismissListener)
+        alert.show()
+    }
+
+    /**
+     *
+     * Create an alert dialog with an input to ask information to the user
+     *
+     * @param context           Current context of the app
+     * @param titleId           Id of the title resource to put in the alert dialog
+     * @param descId            Id of the resource to show as message in the dialog
+     * @param editText          Input to enter information to the user
+     * @param inputType         Type of input for the dialog
+     * @param inputText         Text to put in the edit text selected
+     * @param hintInput         Hint of the edit text
+
+     * @param negativeBtnTextId Hint of the edit text
+     * @return alert dialog
+     */
+    fun createInputDialog(context: Context,
+                          titleId: Int,
+                          descId: Int,
+                          editText: EditText,
+                          inputType: Int,
+                          inputText: String?,
+                          hintInput: Int,
+                          positiveBtnTextId: Int,
+                          negativeBtnTextId: Int): AlertDialog {
+        val layout = LinearLayout(context)
+        val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layout.setPadding(16, 0, 16, 0)
+        layout.layoutParams = params
+
+        editText.setHint(hintInput)
+        editText.setSingleLine(true)
+        editText.inputType = inputType
+        editText.layoutParams = params
+        if (inputText != null) {
+            editText.setText(inputText)
+            editText.setSelection(0, inputText.length)
+        }
+        layout.addView(editText)
+
+        val builder = AlertDialog.Builder(context)
+                .setMessage(descId)
+                .setTitle(titleId)
+                .setView(layout)
+                .setPositiveButton(positiveBtnTextId, null)
+                .setNegativeButton(negativeBtnTextId, null)
+
+        return builder.create()
+    }
+}
