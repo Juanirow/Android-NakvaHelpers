cb3eebe965f06faf4e70d9d90c8c212952222493
Juancho <linanjm90@gmail.com>
2017-11-04 12:04:20 +0000

    new helper to validate the email

---
+++ helpers-nakva/src/main/java/mx/nakva/helpersnakva/EmailValidator.kt
@@ -0,0 +1,26 @@
+package mx.nakva.helpersnakva
+
+import java.util.regex.Pattern
+
+
+/**
+ * Created by Juancho on 04/11/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+object EmailValidator {
+    val REG = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+
+    /**
+     * Verify if the email is an valid email
+     * @param email email to validate
+     * *
+     * @return true if the email is an valid email
+     */
+    fun isValid(email: String): Boolean {
+        val pattern = Pattern.compile(REG)
+        val matcher = pattern.matcher(email)
+
+        return matcher.matches()
+    }
+}
